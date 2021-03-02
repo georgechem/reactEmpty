a02f42191d17309527be585a67aab726e2fdc4e4
root <grzegorzchrymecki@php-developer.ie>
2021-03-02 09:05:03 +0000

    sceleton complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App extends React.Component{
-    render(){
-        return ();
-    }
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>test</h1>
+      </>
+    );
+  }
 }
+
+ReactDOM.render(<App />, document.getElementById("root"));
